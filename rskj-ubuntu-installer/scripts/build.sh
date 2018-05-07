bd5a89afd234a370a0cfbfa2985faa15ceabf4dc
Pablo Chiavarino <pablo@rsk.co>
2018-05-07 16:07:49 -0300

    Changes for release version 0.4.2

--- rskj-ubuntu-installer/scripts/build.sh
+++ rskj-ubuntu-installer/scripts/build.sh
@@ -29,16 +29,16 @@ HOME=$(eval echo "~$USER")
 WORKSPACE=$(echo "$HOME/$VERSION")
 mkdir -p $WORKSPACE/source
 
-mkdir -p $WORKSPACE/source/{zesty,xenial,trusty,artful}/rskj_$VERSION
+mkdir -p $WORKSPACE/source/{bionic,xenial,trusty,artful}/rskj_$VERSION
 
-cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_zesty/. $WORKSPACE/source/zesty/rskj_$VERSION/
+cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_bionic/. $WORKSPACE/source/bionic/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_xenial/. $WORKSPACE/source/xenial/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_trusty/. $WORKSPACE/source/trusty/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_artful/. $WORKSPACE/source/artful/rskj_$VERSION/
 
-sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/zesty/rskj_$VERSION/debian/control
-sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/zesty/rskj_$VERSION/debian/changelog
-sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/zesty/rskj_$VERSION/debian/changelog
+sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/control
+sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/changelog
+sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/changelog
 
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/artful/rskj_$VERSION/debian/control
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/artful/rskj_$VERSION/debian/changelog
@@ -52,16 +52,16 @@ sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/contro
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/changelog
 sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/changelog
 
-cp $FILE_NAME_NODE $WORKSPACE/source/zesty/rskj_$VERSION/src/rsk.jar
+cp $FILE_NAME_NODE $WORKSPACE/source/bionic/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/xenial/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/trusty/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/artful/rskj_$VERSION/src/rsk.jar
 
-cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/zesty/rskj_$VERSION/src/regtest.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/zesty/rskj_$VERSION/src/mainnet.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/zesty/rskj_$VERSION/src/testnet.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/zesty/rskj_$VERSION/src/
-cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk.service-node $WORKSPACE/source/zesty/rskj_$VERSION/src/rsk.service
+cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/regtest.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/mainnet.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/testnet.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/bionic/rskj_$VERSION/src/
+cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk.service-node $WORKSPACE/source/bionic/rskj_$VERSION/src/rsk.service
 
 cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/artful/rskj_$VERSION/src/regtest.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/artful/rskj_$VERSION/src/mainnet.conf
@@ -82,9 +82,9 @@ cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/trusty
 cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk-node $WORKSPACE/source/trusty/rskj_$VERSION/src/rsk
 
 echo "####################################################"
-echo "#                   BUILD ZESTY                    #"
+echo "#                   BUILD BIONIC                    #"
 echo "####################################################"
-cd $WORKSPACE/source/zesty/rskj_$VERSION
+cd $WORKSPACE/source/bionic/rskj_$VERSION
 debuild -us -uc -S
 
 echo "####################################################"
@@ -106,9 +106,9 @@ cd $WORKSPACE/source/trusty/rskj_$VERSION
 debuild -us -uc -S
 cd $WORKSPACE/source/xenial
 
-mkdir -p $WORKSPACE/build/{xenial,trusty,zesty,artful}
+mkdir -p $WORKSPACE/build/{xenial,trusty,bionic,artful}
 
 mv $WORKSPACE/source/trusty/rskj_$VERSION* $WORKSPACE/build/trusty/
 mv $WORKSPACE/source/xenial/rskj_$VERSION* $WORKSPACE/build/xenial/
-mv $WORKSPACE/source/zesty/rskj_$VERSION* $WORKSPACE/build/zesty/
+mv $WORKSPACE/source/bionic/rskj_$VERSION* $WORKSPACE/build/zesty/
 mv $WORKSPACE/source/artful/rskj_$VERSION* $WORKSPACE/build/artful/
