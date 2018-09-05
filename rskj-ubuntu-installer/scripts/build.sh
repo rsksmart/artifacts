b71fc6977931e64bbf9a0ca7c0d4f3d9b295466a
Pablo Chiavarino <pablo@rsk.co>
2018-09-05 13:21:37 -0300

    Remove Artful from build script

--- rskj-ubuntu-installer/scripts/build.sh
+++ rskj-ubuntu-installer/scripts/build.sh
@@ -34,15 +34,10 @@ mkdir -p $WORKSPACE/source/{bionic,xenial,trusty,artful}/rskj_$VERSION
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_bionic/. $WORKSPACE/source/bionic/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_xenial/. $WORKSPACE/source/xenial/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_trusty/. $WORKSPACE/source/trusty/rskj_$VERSION/
-cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_artful/. $WORKSPACE/source/artful/rskj_$VERSION/
 
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/control
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/changelog
 sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/changelog
-
-sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/artful/rskj_$VERSION/debian/control
-sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/artful/rskj_$VERSION/debian/changelog
-sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/artful/rskj_$VERSION/debian/changelog
 
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/trusty/rskj_$VERSION/debian/control
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/trusty/rskj_$VERSION/debian/changelog
@@ -55,19 +50,12 @@ sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/cha
 cp $FILE_NAME_NODE $WORKSPACE/source/bionic/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/xenial/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/trusty/rskj_$VERSION/src/rsk.jar
-cp $FILE_NAME_NODE $WORKSPACE/source/artful/rskj_$VERSION/src/rsk.jar
 
 cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/regtest.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/mainnet.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/testnet.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/bionic/rskj_$VERSION/src/
 cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk.service-node $WORKSPACE/source/bionic/rskj_$VERSION/src/rsk.service
-
-cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/artful/rskj_$VERSION/src/regtest.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/artful/rskj_$VERSION/src/mainnet.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/artful/rskj_$VERSION/src/testnet.conf
-cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/artful/rskj_$VERSION/src/
-cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk.service-node $WORKSPACE/source/artful/rskj_$VERSION/src/rsk.service
 
 cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/xenial/rskj_$VERSION/src/testnet.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/xenial/rskj_$VERSION/src/regtest.conf
@@ -88,12 +76,6 @@ cd $WORKSPACE/source/bionic/rskj_$VERSION
 debuild -us -uc -S
 
 echo "####################################################"
-echo "#                   BUILD ARTFUL                   #"
-echo "####################################################"
-cd $WORKSPACE/source/artful/rskj_$VERSION
-debuild -us -uc -S
-
-echo "####################################################"
 echo "#                   BUILD XENIAL                   #"
 echo "####################################################"
 cd $WORKSPACE/source/xenial/rskj_$VERSION
@@ -106,9 +88,8 @@ cd $WORKSPACE/source/trusty/rskj_$VERSION
 debuild -us -uc -S
 cd $WORKSPACE/source/xenial
 
-mkdir -p $WORKSPACE/build/{xenial,trusty,bionic,artful}
+mkdir -p $WORKSPACE/build/{xenial,trusty,bionic}
 
 mv $WORKSPACE/source/trusty/rskj_$VERSION* $WORKSPACE/build/trusty/
 mv $WORKSPACE/source/xenial/rskj_$VERSION* $WORKSPACE/build/xenial/
 mv $WORKSPACE/source/bionic/rskj_$VERSION* $WORKSPACE/build/bionic/
-mv $WORKSPACE/source/artful/rskj_$VERSION* $WORKSPACE/build/artful/
