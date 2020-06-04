53c660f96963625ada9d3931640dc6c80b94d90b
Pablo Chiavarino <pablo@rsk.co>
2020-06-04 15:48:50 -0300

    script changes and folder structure

--- rskj-ubuntu-installer/scripts/build.sh
+++ rskj-ubuntu-installer/scripts/build.sh
@@ -29,11 +29,16 @@ HOME=$(eval echo "~$USER")
 WORKSPACE=$(echo "$HOME/$VERSION")
 mkdir -p $WORKSPACE/source
 
-mkdir -p $WORKSPACE/source/{bionic,xenial,trusty,artful}/rskj_$VERSION
+mkdir -p $WORKSPACE/source/{bionic,xenial,trusty,focal}/rskj_$VERSION
 
+cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_focal/. $WORKSPACE/source/trusty/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_bionic/. $WORKSPACE/source/bionic/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_xenial/. $WORKSPACE/source/xenial/rskj_$VERSION/
 cp -r ~/artifacts/rskj-ubuntu-installer/rskj_package_trusty/. $WORKSPACE/source/trusty/rskj_$VERSION/
+
+sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/focal/rskj_$VERSION/debian/control
+sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/focal/rskj_$VERSION/debian/changelog
+sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/focal/rskj_$VERSION/debian/changelog
 
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/control
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/bionic/rskj_$VERSION/debian/changelog
@@ -47,9 +52,16 @@ sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/contro
 sed -i "s|<V>|${VERSION}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/changelog
 sed -i "s|<DATE>|${RFCDATE}|g" $WORKSPACE/source/xenial/rskj_$VERSION/debian/changelog
 
+cp $FILE_NAME_NODE $WORKSPACE/source/focal/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/bionic/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/xenial/rskj_$VERSION/src/rsk.jar
 cp $FILE_NAME_NODE $WORKSPACE/source/trusty/rskj_$VERSION/src/rsk.jar
+
+cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/focal/rskj_$VERSION/src/regtest.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/focal/rskj_$VERSION/src/mainnet.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/testnet.conf $WORKSPACE/source/focal/rskj_$VERSION/src/testnet.conf
+cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/focal/rskj_$VERSION/src/
+cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk.service-node $WORKSPACE/source/focal/rskj_$VERSION/src/rsk.service
 
 cp ~/artifacts/rskj-ubuntu-installer/config/regtest.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/regtest.conf
 cp ~/artifacts/rskj-ubuntu-installer/config/mainnet.conf $WORKSPACE/source/bionic/rskj_$VERSION/src/mainnet.conf
@@ -70,6 +82,12 @@ cp ~/artifacts/rskj-ubuntu-installer/config/logback.xml $WORKSPACE/source/trusty
 cp ~/artifacts/rskj-ubuntu-installer/init-scripts/rsk-node $WORKSPACE/source/trusty/rskj_$VERSION/src/rsk
 
 echo "####################################################"
+echo "#                   BUILD FOCAL                    #"
+echo "####################################################"
+cd $WORKSPACE/source/focal/rskj_$VERSION
+debuild -us -uc -S
+
+echo "####################################################"
 echo "#                   BUILD BIONIC                    #"
 echo "####################################################"
 cd $WORKSPACE/source/bionic/rskj_$VERSION
@@ -86,10 +104,10 @@ echo "#                   BUILD TRUSTY                   #"
 echo "####################################################"
 cd $WORKSPACE/source/trusty/rskj_$VERSION
 debuild -us -uc -S
-cd $WORKSPACE/source/xenial
 
-mkdir -p $WORKSPACE/build/{xenial,trusty,bionic}
+mkdir -p $WORKSPACE/build/{xenial,trusty,bionic,focal}
 
 mv $WORKSPACE/source/trusty/rskj_$VERSION* $WORKSPACE/build/trusty/
 mv $WORKSPACE/source/xenial/rskj_$VERSION* $WORKSPACE/build/xenial/
 mv $WORKSPACE/source/bionic/rskj_$VERSION* $WORKSPACE/build/bionic/
+mv $WORKSPACE/source/bionic/rskj_$VERSION* $WORKSPACE/build/focal/
